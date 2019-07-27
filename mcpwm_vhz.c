f98ac8365b548ee9ea2d9b4901875e6ead48d2d5
ajpina <ajpina@gmail.com>
2019-07-27 07:11:17 -0400

    pole pairs scaling of fake phase

--- mcpwm_vhz.c
+++ mcpwm_vhz.c
@@ -2129,7 +2129,7 @@ void mcpwm_vhz_adc_int_handler(void *p, uint32_t flags) {
 		// Allow start in SPEED CONTROL
 		if (m_control_mode == CONTROL_MODE_SPEED || m_control_mode == CONTROL_MODE_POS) {
 			static float fake_angle = 0.0;
-			fake_angle += dt * m_speed_pid_set_rpm * M_PI / 30.0;
+			fake_angle += dt * (m_speed_pid_set_rpm / m_conf->si_motor_poles / 2.0) * M_PI / 30.0;
 			utils_norm_angle_rad(&fake_angle);
 			m_motor_state.phase_fake = fake_angle;
 		}
